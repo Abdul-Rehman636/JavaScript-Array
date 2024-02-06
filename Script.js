5a9f70ee7766bf1038699b82876c7f39f022aacd
Abdul-Rehman636 <rehmankhan636@gmail.com>
2024-02-07 00:04:03 +0500

    JavaScript Array

---
+++ Script.js
@@ -0,0 +1,49 @@
+// Array Methods
+
+//1 ToString Method
+
+var a = ["Apple", "Banana", "Mango"];
+
+var b = a.toString();
+
+console.log(b);
+
+//2 Join() Method
+
+var c = ["Car", "Bike"];
+
+var d = c.join("-");
+
+console.log(d);
+
+//3 Pop() Method
+
+var e = ["1", "3", "5", "7", "9"];
+
+var f = e.pop();
+
+console.log(e, f);
+
+//4 Push() Method
+
+var g = ["4", "2", "6", "8"];
+
+var h = g.push(10);
+
+console.log(g ,h);
+
+//5 Shift() Method
+
+var i = ["2","4","6","8","10"];
+
+var j = i.shift();
+
+console.log(j, i);
+
+//6 Unshift() Method
+
+var k = ["2", "4", "6", "8"];
+
+var l = k.unshift(1);
+
+console.log(l, k);
